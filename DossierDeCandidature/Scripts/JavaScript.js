799f9ea90381220e2f94572cc0cba29552cb1730
armelle tamo <armelletamo@yahoo.fr>
2018-05-09 10:35:01 +0200

    modification du javascript

--- DossierDeCandidature/Scripts/JavaScript.js
+++ DossierDeCandidature/Scripts/JavaScript.js
@@ -4,7 +4,7 @@
 
         var x = document.getElementsByClassName("champs-competence").length;
 
-        var html = "<br /><div class='champs-competence'><div class='form-group'><label class='control-label'> Compétence</label > <div><textarea name='[" + x + "].Competence' cols='60' rows='2' class='form-control'></textarea></div></div > <div class='form-group'><label class='control-label'>Niveau</label><div><select class='form-control text-center' name='[" + x + "].NiveauCompetence'><option value=''> - Niveau - </option><option value='Debutant'> Debutant (0 à 1 an) </option><option value='Confirme'>Confirmé (2 à 5 ans)</option><option value='Experimente'>Expérimenté (plus de 5 ans)</option ></select ></div ></div ></div > ";
+        var html = "<div class='champs-competence'><div class='form-group'><label class='control-label'> Compétence</label > <div><textarea name='[" + x + "].Competence' cols='60' rows='1' class='form-control'></textarea></div></div > <div class='form-group'><label class='control-label'>Niveau</label><div><select class='form-control text-center' name='[" + x + "].NiveauCompetence'><option value=''> - Niveau - </option><option value='Debutant'> Debutant (0 à 1 an) </option><option value='Confirme'>Confirmé (2 à 5 ans)</option><option value='Experimente'>Expérimenté (plus de 5 ans)</option ></select ></div ></div ></div > ";
         $("#d").append(html);
     });
 });
@@ -16,7 +16,7 @@ $(document).ready(function () {
 
         var x = document.getElementsByClassName("Champ_langue").length;
 
-        var html = '<div class="Champ_langue"><div class="form-group"><label>Autre</label><input class="form-control" name="[' + x + '].Langue"/> </div><div class="form-group"><label class="control-label">Niveau Langue</label><select style="text-align-last:center" class="form-control" name="[' + x + '].NiveauLangue"><option value="Non opérationnel"> Non opérationnel </option><option value="Opérationnel">Opérationnel</option><option value="Courant">Courant</option></select></div></div>';
+        var html = '<div class="Champ_langue"><div class="form-group"><label>Autre</label><div><input class="form-control"  name="[' + x + '].Langue"/></div></div><div class="form-group"><label class="control-label">Niveau Langue</label><select style="text-align-last:center" class="form-control" name="[' + x + '].NiveauLangue"><option value="Non opérationnel"> Non opérationnel </option><option value="Opérationnel">Opérationnel</option><option value="Courant">Courant</option></select></div></div>';
         $("#l").append(html);
     });
 });
@@ -26,7 +26,7 @@ $(document).ready(function () {
 
     $("#btn1").click(function () {
         var y = document.getElementsByClassName("champs").length;
-        var html = "<br /><br /><div class='champs'><div class='form-group'><label class='control-label'>Nom/Prénom</label><div><input name='[" + y + "].NomPrenom' class='form-control'/></div></div><div class='form-group'><label class='control-label'>Fonction</label><div><input name='[" + y + "].Fonction' class='form-control'/></div></div><br/><div class='form-group'><label class='control-label'>Sociéte</label><div><input name='[" + y + "].Societe' class='form-control'/></div></div><div class='form-group'><label class='control-label'>Tel/Mail</label><div><input name='[" + y + "].TelMail' class='form-control'/></div></div></div>";
+        var html = "<div class='champs'><div class='form-group'><label class='control-label'>Nom/Prénom</label><div><input name='[" + y + "].NomPrenom' class='form-control'/></div></div><div class='form-group'><label class='control-label'>Fonction</label><div><input name='[" + y + "].Fonction' class='form-control'/></div></div><div class='form-group'><label class='control-label'>Sociéte</label><div><input name='[" + y + "].Societe' class='form-control'/></div></div><div class='form-group'><label class='control-label'>Tel/Mail</label><div><input name='[" + y + "].TelMail' class='form-control'/></div></div></div>";
         $("#d1").append(html);
     });
 });

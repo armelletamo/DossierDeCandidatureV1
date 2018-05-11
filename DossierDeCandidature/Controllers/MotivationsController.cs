f154d168fb0265a41e57a0fcfa94242ddf749f96
hedi77 <lachtane.h@gmail.com>
2018-05-11 14:49:24 +0200

    Modification try catch

--- DossierDeCandidature/Controllers/MotivationsController.cs
+++ DossierDeCandidature/Controllers/MotivationsController.cs
@@ -29,43 +29,51 @@ namespace DossierDeCandidature.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,ExperisIt,MotifRecherche,Objectif")] Motivation motivation)
         {
-            if (ModelState.IsValid)
+            try
             {
-                if (Session["administratif"] == null)
+                if (ModelState.IsValid)
                 {
-                    ViewBag.messageExpirationSession = "La session a expiré veuillez resaisir vos informations";
-                    return View("~/Views/Home/Index.cshtml");
+                    if (Session["administratif"] == null)
+                    {
+                        ViewBag.messageExpirationSession = "La session a expiré veuillez resaisir vos informations";
+                        return View("~/Views/Home/Index.cshtml");
+                    }
+                    RenseignementAdministratif candidatures = (RenseignementAdministratif)Session["administratif"];
+                    candidatures.Motivation = motivation;
+                    Session["administratif"] = candidatures;
+                    return RedirectToAction("Enregistrement", "Enregistrement");
+
                 }
-                RenseignementAdministratif candidatures = (RenseignementAdministratif)Session["administratif"];                
-                candidatures.Motivation = motivation;
-                Session["administratif"] = candidatures;
-                return RedirectToAction("Enregistrement", "Enregistrement");
-
+                return View(motivation);
             }
-
-            return View(motivation);
+            catch
+            {
+                return HttpNotFound();
+            }
         }
 
         // GET: Motivations/Edit/5
         public async Task<ActionResult> Edit(string id)
         {
-
-            int? ID = BitConverter.ToInt32(Convert.FromBase64String(id + "=="), 0);
-            if (Session["idRenseignement"] == null)
+            try
+            {
+                int? ID = BitConverter.ToInt32(Convert.FromBase64String(id + "=="), 0);
+                int Id = (int)Session["idRenseignement"];
+                if (ID == null && ID != Id)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Motivation motivation = await db.Motivations.FindAsync(ID);
+                if (motivation == null)
+                {
+                    return HttpNotFound();
+                }
+                return View(motivation);
+            }
+            catch
             {
                 return HttpNotFound();
             }
-            int Id = (int)Session["idRenseignement"];
-            if (ID == null && ID != Id)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Motivation motivation = await db.Motivations.FindAsync(ID);
-            if (motivation == null)
-            {
-                return HttpNotFound();
-            }
-            return View(motivation);
         }
 
         // POST: Motivations/Edit/5
@@ -75,19 +83,22 @@ namespace DossierDeCandidature.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,ExperisIt,MotifRecherche,Objectif")] Motivation motivation)
         {
-            if (Session["idRenseignement"] == null)
+            try
+            {
+                int Id = (int)Session["idRenseignement"];
+                string NewID = Convert.ToBase64String(BitConverter.GetBytes(Id)).Replace("==", "");
+                if (ModelState.IsValid)
+                {
+                    db.Entry(motivation).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return RedirectToAction("Verification", "Enregistrement", new { id = NewID });
+                }
+                return View(motivation);
+            }
+            catch
             {
                 return HttpNotFound();
             }
-            int Id = (int)Session["idRenseignement"];
-            string NewID = Convert.ToBase64String(BitConverter.GetBytes(Id)).Replace("==", "");
-            if (ModelState.IsValid)
-            {
-                db.Entry(motivation).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Verification", "Enregistrement", new { id = NewID });
-            }
-            return View(motivation);
         }
 
 
